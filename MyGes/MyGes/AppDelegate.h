d8dfe04982c1f5632e3ab2cfbecd1d96aa56a7ae
陈雅静 <chenyajing@gessic-2.local>
2015-10-17 00:02:55 +0800

    初始化测试项目

---
+++ MyGes/MyGes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGes
+//
+//  Created by 陈雅静 on 15/10/17.
+//  Copyright (c) 2015年 yj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyGes/MyGes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGes
+//
+//  Created by 陈雅静 on 15/10/17.
+//  Copyright (c) 2015年 yj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
